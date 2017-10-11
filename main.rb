3d11c3469c3a2f093ff40abb1b82136ff372b168
johshisha <johshisha@gmail.com>
2017-10-11 20:37:54 +0900

    Run on pty

--- main.rb
+++ main.rb
@@ -1,6 +1,10 @@
 require 'slack'
 require 'pry'
 require 'dotenv'
+require 'pty'
+require 'expect'
+
+require 'pry'
 
 Dotenv.load
 
@@ -11,12 +15,32 @@ end
 
 client = Slack.realtime
 
+$expect_verbose = true
+$cmd_result = ""
+$output, input, pid = PTY.spawn('bash')
+
+def run_shell
+  while true do
+    ret = $output.readline
+    $cmd_result << ret
+  end
+end
+
+Thread.new { run_shell }
+
 client.on :message do |data|
   if not data['username'] == 'ssh_bot' then
     cmd = data['text'].gsub('&amp;', '&')
     p cmd
-    ret = `#{cmd}`
-    Slack.chat_postMessage(text: "```$ #{cmd}\n#{ret}```", channel: 'ssh')
+    input.puts cmd
+    if cmd.gsub(/[\n|\r]/, "") == 'exit' then
+      exit 0
+    end
+    sleep 1
+    ret = $cmd_result.slice($cmd_result.index("$"), $cmd_result.size)
+    $cmd_result = ""
+    # p "#{ret}"
+    Slack.chat_postMessage(text: "```#{ret}```", channel: 'ssh')
   end
 end
 
